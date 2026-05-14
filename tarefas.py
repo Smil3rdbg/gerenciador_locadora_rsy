42861b3ba1bf611eba387fc005ffd4adf36878b2
Smil3rdbg <yasminsantosdelima57@gmail.com>
2026-05-14 08:50:20 -0300

    sara ajustes

---
+++ tarefas.py
@@ -0,0 +1,5 @@
+#casdastro de filme e livros , cadastro usuario, validacao de senha e email, Apagar divida, sistema de compra/aluguel (fita, comida,merch,videocassete) e devolucao, ifood krlh,
+
+filme_n = input('Digite o nome do filme:')
+
+livro_n = input('Digite o nome do filme')
