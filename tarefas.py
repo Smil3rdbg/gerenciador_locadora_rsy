14017c70d6928a05aa66e59f0164dde2c44a1067
sarasoliv <saradsoliveira1@gmail.com>
2026-05-14 21:16:18 -0300

    sara ajustes 2.0

--- tarefas.py
+++ tarefas.py
@@ -1,5 +1,20 @@
 #casdastro de filme e livros , cadastro usuario, validacao de senha e email, Apagar divida, sistema de compra/aluguel (fita, comida,merch,videocassete) e devolucao, ifood krlh,
 
-filme_n = input('Digite o nome do filme:')
+#funcoes
 
-livro_n = input('Digite o nome do filme')
+def cadastro_filme():
+    filme = {}  #criou um dicionário vazio para armazenar as informações do filme
+    filme['titulo'] = input('Digite o título do filme: ')
+    filme['diretor'] = input('Digite o nome do diretor: ')  # filme = dicionario [titulo] = cria uma etiqueta, topico com esse nome
+    filme['ano'] = input('Digite o ano de lançamento: ')
+    print(f'\n--------------------------------------------------\nTítulo: {filme["titulo"]}\nDiretor: {filme["diretor"]}\nAno de lançamento: {filme["ano"]}\n\n-----------Filme cadastrado com sucesso!----------')  
+    return filme   #retorna o dicionário preenchido com as informações do filme e finaliza a execucao codigo acima assim mostrando o "resultado"
+
+
+def cadastro_livro():
+    livro = {} 
+    livro['titulo'] = input('Digite o título do livro: ')
+    livro['autor'] = input('Digite o nome do autor: ')  
+    livro['ano'] = input('Digite o ano de lançamento: ')
+    print(f'\n--------------------------------------------------\nTítulo: {livro["titulo"]}\nAutor: {livro["autor"]}\nAno de lançamento: {livro["ano"]}\n\n-----------Livro cadastrado com sucesso!----------')  
+    return livro  
