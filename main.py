85349c881e6a74524d13b182535da351b73d3ad7
sarasoliv <saradsoliveira1@gmail.com>
2026-05-15 19:22:37 -0300

    sara 1.0

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 bom dia rodrygo <3 
 tkinter
+
+from tarefas import cadastro_filme, cadastro_livro, cadastro_hq, ver_infos_filme, ver_infos_livro, ver_infos_hq, lista_filme, lista_livro, lista_hq, apagar_cadastrofilme
+
+# tem q colocar ate a variavel da lista pra importar 
+
+
+cadastro_filme() 
+
+
+
+ver_infos_filme(lista_filme)
+
+
+#APAGAR CADASTRO DE FILME
+print('\n---------EXLUSÃO DE CADASTRO DE FILME---------')
+print('1 - Sim\n2 - Não')
+opcao_apagar = int(input('\nDeseja apagar o cadastro?: '))
+
+if opcao_apagar == 1:
+    apagar_cadastrofilme(lista_filme)
+elif opcao_apagar == 2:
+    print('Cadastro mantido!')
+    ver_infos_filme(lista_filme)
+else:
+    print('Opção invalida!')
+
+
+
+
+
+
