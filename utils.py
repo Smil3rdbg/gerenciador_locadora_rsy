8ec7d517e1045487cf6f3a87c67304a414c5640a
sarasoliv <saradsoliveira1@gmail.com>
2026-05-15 22:54:32 -0300

    sara 1.0

--- utils.py
+++ utils.py
@@ -1,1 +1,16 @@
 #sistema multa, deixar bonitinho/organizado, configuro aparencia data, sistema de apagar e salvar(tuplas), configurar dinheiro pra float.  
+
+#LISTAS 
+
+lista_filme = [
+    {
+        "id": 9864344,
+        "titulo": "Sara",
+        "diretor": "linda",
+        "ano": "maravilhosa"
+    }
+]  #lista com infos pra testar
+
+lista_livro = []  #cria uma lista vazia para armazenar os livros cadastrados
+
+lista_hq = [] 
