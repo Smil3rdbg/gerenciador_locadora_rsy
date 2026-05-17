83d9258a93761a11f0c844ed3001293360a1cd73
sarasoliv <saradsoliveira1@gmail.com>
2026-05-17 15:39:22 -0300

    sara 5.0

--- tarefas.py
+++ tarefas.py
@@ -1,81 +1,140 @@
 #casdastro de filme e livros , cadastro usuario, validacao de senha e email, Apagar divida, sistema de compra/aluguel (fita, comida,merch,videocassete) e devolucao, ifood de fita, sistema de estoque feito em pilha
 
-from utils import lista_filme, lista_livro, lista_hq
+from utils import chama_id, lista_filme, lista_livro, lista_hq
 import random
 
-id = random.randint(1000, 9999999)  #gera um número aleatório entre 1000 e 9999999 para ser usado como ID do filme, livro ou HQ cadastrado
+
+   
 
 #FUNÇÕES
 
 def cadastro_filme():
     filme = {} #criou um dicionário vazio para armazenar as informações do filme
-    filme['id'] = id  #atribui o ID gerado ao filme
-    filme['titulo'] = input('Digite o título do filme: ')
-    filme['diretor'] = input('Digite o nome do diretor: ')  # filme = dicionario [titulo] = cria uma etiqueta, topico com esse nome
-    filme['ano'] = input('Digite o ano de lançamento: ')
-
+    filme['id'] = chama_id()  #atribui o ID gerado ao filme
+    filme['titulo'] = input('\nDigite o TÍTULO do filme: ')
+    filme['genero'] = input('\nDigite o GÊNERO do filme: ')
+    while True : # (loop) enquanto for verdade que o input não é int ele vai ficar em looping
+        estoque = input('\nDigite a QUANTIDADE de exemplares em ESTOQUE: ') #tive que criar a variavel estoque fora do dic pra atribuir o dado numero caso estivesse sido colocado
+        if estoque.isdigit(): #o valor dado é int?
+            filme['estoque'] = int(estoque)
+            break # quebra o loop caso o usuario digite um número válido para o estoque
+        else :  
+            print('\nValor inválido para estoque. Por favor, digite um número.')
+    filme['estudio'] = input('\nDigite o nome do ESTÚDIO: ') # o problema era aq q o estudio tava dentro do loop do estoque, ai eu separei em 3 etapar assim colocando ua folha no meio de tijolos
+    while True :
+        ano = input('\nDigite o ANO de LANÇAMENTO: ')
+        if ano.isdigit():
+            filme['ano'] = int(ano)
+            lista_filme.append(filme)  #adiciona o dicionário do filme à lista de filmes cadastrados
+            ver_infos_filme(lista_filme) #retorna o dicionário preenchido com as informações do filme e finaliza a execucao codigo acima assim mostrando o "resultado"
+            break
+        else :  
+            print('\nValor inválido para ano de lançamento. Por favor, digite um número.')
 # chaves juntas n usam virgula pra separar os inputs, chaves q inglobam os msms precisam ser separados por virgula.
-
-    lista_filme.append(filme)  #adiciona o dicionário do filme à lista de filmes cadastrados
-    print(f'\n--------------------------------------------------\nID: {filme["id"]}\nTítulo: {filme["titulo"]}\nDiretor: {filme["diretor"]}\nAno de lançamento: {filme["ano"]}\n\n-----------Filme cadastrado com sucesso!----------')  
-    return filme   #retorna o dicionário preenchido com as informações do filme e finaliza a execucao codigo acima assim mostrando o "resultado"
 
 
 def cadastro_livro():
     livro = {} 
-    livro['id'] = id
-    livro['titulo'] = input('Digite o título do livro: ')
-    livro['autor'] = input('Digite o nome do autor: ')  
-    livro['ano'] = input('Digite o ano de lançamento: ')
-    lista_livro.append(livro)
-    print(f'\n--------------------------------------------------\nID: {livro["id"]}\nTítulo: {livro["titulo"]}\nAutor: {livro["autor"]}\nAno de lançamento: {livro["ano"]}\n\n-----------Livro cadastrado com sucesso!----------')  
-    return livro  
+    livro['id'] = chama_id()  
+    livro['titulo'] = input('\nDigite o TÍTULO do livro: ')
+    livro['genero'] = input('\nDigite o GÊNERO do livro: ')
+    while True : 
+        estoque = input('\nDigite a QUANTIDADE de exemplares em ESTOQUE: ') 
+        if estoque.isdigit(): 
+            livro['estoque'] = int(estoque)
+            break 
+        else :  
+            print('\nValor inválido para estoque. Por favor, digite um número.')
+    livro['estudio'] = input('\nDigite o nome do ESTÚDIO: ') 
+    while True :
+        ano = input('\nDigite o ANO de LANÇAMENTO: ')
+        if ano.isdigit():
+            livro['ano'] = int(ano)
+            lista_livro.append(livro)  
+            ver_infos_livro(lista_livro)
+            break
+        else :  
+            print('\nValor inválido para ano de lançamento. Por favor, digite um número.')
 
 
 def cadastro_hq():
     hq = {} 
-    hq['id'] = id
-    hq['titulo'] = input('Digite o título da HQ: ')
-    hq['autor'] = input('Digite o nome do autor: ')  
-    hq['ano'] = input('Digite o ano de lançamento: ')
-    lista_hq.append(hq)
-    print(f'\n--------------------------------------------------\nID: {hq["id"]}\nTítulo: {hq["titulo"]}\nAutor: {hq["autor"]}\nAno de lançamento: {hq["ano"]}\n\n-----------HQ cadastrada com sucesso!----------')  
-    return hq  
+    hq['id'] = chama_id()  
+    hq['titulo'] = input('\nDigite o TÍTULO da HQ: ')
+    hq['genero'] = input('\nDigite o GÊNERO da HQ: ')
+    while True : 
+        estoque = input('\nDigite a QUANTIDADE de exemplares em ESTOQUE: ') 
+        if estoque.isdigit(): 
+            hq['estoque'] = int(estoque)
+            break 
+        else :  
+            print('\nValor inválido para estoque. Por favor, digite um número.')
+    hq['estudio'] = input('\nDigite o nome do ESTÚDIO: ') 
+    while True :
+        ano = input('\nDigite o ANO de LANÇAMENTO: ')
+        if ano.isdigit():
+            hq['ano'] = int(ano)
+            lista_hq.append(hq)  
+            ver_infos_hq(lista_hq)
+            break
+        else :  
+            print('\nValor inválido para ano de lançamento. Por favor, digite um número.')
+            
 
 
 
 #APAGAR ITENS CADASTRADOS
 
 def apagar_cadastrofilme(lista_filme):
-    
-        id_para_apagar = int(input('\n\nDigite o ID do filme que deseja apagar: '))  #PERGUNTA PRIMEIRO (Fora do loop, para perguntar uma vez só)
-        
-        for filme in lista_filme: #PARA variavel (dicionario) NA lista de filmes FAÇA:
-            if id_para_apagar == filme['id']: #SE o id digitado for igual ao id do filme na lista ENTÃO:
-                lista_filme.remove(filme)
-            else:    
-                print('\n\nID não encontrado! Tente novamente.\n\n') 
-                return #remove o filme da lista de filmes 
-        
+        while True:
+            id_para_apagar = input('\n\nDigite o ID do filme que deseja apagar: ')
+            
+            filme_achado = False
+            for filme in lista_filme: #PARA variavel (dicionario) NA lista de filmes FAÇA:
+                if id_para_apagar.isdigit() and int(id_para_apagar) == filme['id']: #SE o id digitado for igual ao id do filme na lista ENTÃO: 
+                    lista_filme.remove(filme)
+                    print('\n\nCadastro apagado com sucesso!\n\n')
+                    filme_achado = True
+                    return
+
+                if not filme_achado :  
+                    print('\n\nID não encontrado! Tente novamente.\n') 
+                
+# pede o input, cria uma variavel e coloca ela como false, puxa o dicionario filme da lista, se id NÂO(ta false) for str(letra) e ao se tornar int se numero for igual o id de algum filme, ele remove tudo do filme, mostra q removeu, transforma a variavel em true e para todos os loopings
+# se o id (ta false originalmente) NÃO (muda pra true) (ai teria que ser: for str ou for um numero q n é um id) vai encotrar id e vai voltar pro começo do loop.
+
+
 def apagar_cadastrolivro(lista_livro):
     
-        id_para_apagar = int(input('\n\nDigite o ID do livro que deseja apagar: '))  
-        
-        for livro in lista_livro: 
-            if id_para_apagar == livro['id']: 
-                lista_livro.remove(livro)
-            else:    
-                print('\n\nID não encontrado! Tente novamente.\n\n') 
-                return 
+        while True:
+            id_para_apagar = input('\n\nDigite o ID do livro que deseja apagar: ')
+            
+            livro_achado = False
+            for livro in lista_livro: 
+                if id_para_apagar.isdigit() and int(id_para_apagar) == livro['id']: 
+                    lista_livro.remove(livro)
+                    print('\n\nCadastro apagado com sucesso!\n\n')
+                    livro_achado = True
+                    return
+
+                if not livro_achado :  
+                    print('\n\nID não encontrado! Tente novamente.\n') 
         
 def apagar_cadastrohq(lista_hq):
     
-        id_para_apagar = int(input('\n\nDigite o ID da HQ que deseja apagar: '))  
-        
-        for hq in lista_hq: 
-            if id_para_apagar == hq['id']: 
-                lista_hq.remove(hq)
-            else:    
+        while True:
+            id_para_apagar = input('\n\nDigite o ID da HQ que deseja apagar: ')
+            
+            hq_achado = False
+            for hq in lista_hq: 
+                if id_para_apagar.isdigit() and int(id_para_apagar) == hq['id']:  
+                    lista_hq.remove(hq)
+                    print('\n\nCadastro apagado com sucesso!\n\n')
+                    hq_achado = True
+                    return
+
+                if not hq_achado :  
+                    print('\n\nID não encontrado! Tente novamente.\n') 
                 print('\n\nID não encontrado! Tente novamente.\n\n') 
                 return 
         
@@ -86,17 +145,18 @@ def apagar_cadastrohq(lista_hq):
 
 def ver_infos_filme(lista_filme):
     print("\n---------FILMES CADASTRADOS:--------\n")
-    for filme in lista_filme:  #LOOP PARA PERCORRER A LISTA DE FILMES CADASTRADOS E IMPRIMIR AS INFORMAÇÕES DE CADA UM
-        print(filme)
+    for filme in lista_filme:  #LOOP PARA PERCORRER A LISTA DE FILMES CADASTRADOS E IMPRIMIR AS INFORMAÇÕES DE CADA Um
+        print(f'\n📍 ID: {filme["id"]}\n\n📎 Título: {filme["titulo"]}\n\n🎞️  Gênero: {filme["genero"]}\n\n📦 Em estoque: {filme["estoque"]}\n\n🎬 Estúdio: {filme["estudio"]}\n\n🎆 Ano de lançamento: {filme["ano"]}') #printa as infos pra eu conseguir deixar organizado, c eu printasse apenas (filme) ia puxar tudo da lista e ia ficar feio
+        print('=' * 50)  #adiciona 50 = para separar visualmente as informações de cada filme cadastrado
 
 def ver_infos_livro(lista_livro):
     print("\n---------LIVROS CADASTRADOS:--------\n")
     for livro in lista_livro:
-        print(livro)
-
+        print(f'\n📍 ID: {livro["id"]}\n\n📎 Título: {livro["titulo"]}\n\n🎞️  Gênero: {livro["genero"]}\n\n📦 Em estoque: {livro["estoque"]}\n\n🎬 Estúdio: {livro["estudio"]}\n\n🎆 Ano de lançamento: {livro["ano"]}')
+        print('=' * 50)  
+        
 def ver_infos_hq(lista_hq):
     print("\n---------HQS CADASTRADAS:--------\n")
     for hq in lista_hq:
-        print(hq)
-
-
+        print(f'\n📍 ID: {hq["id"]}\n\n📎 Título: {hq["titulo"]}\n\n🎞️  Gênero: {hq["genero"]}\n\n📦 Em estoque: {hq["estoque"]}\n\n🎬 Estúdio: {hq["estudio"]}\n\n🎆 Ano de lançamento: {hq["ano"]}')
+        print('=' * 50)  
