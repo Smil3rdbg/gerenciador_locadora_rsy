6ff8e21ae796d2815076da59f5c17dc7cf0e17a1
sarasoliv <saradsoliveira1@gmail.com>
2026-05-17 15:31:50 -0300

    sara 2.0

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 #sistema multa, deixar bonitinho/organizado, configuro aparencia data, sistema de apagar e salvar(tuplas), configurar dinheiro pra float.  
 
-#LISTAS 
+#LISTAS .
 
 #entre '' é str sem é int
 
